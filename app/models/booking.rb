a6befbab04bde350de01370fa50087d5057fe927
Marek Polakowski <marek.polakowski@gmail.com>
2022-01-18 10:46:30 +0100

    feat: booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,34 @@ class Booking < ApplicationRecord
 
   validates :date,
             presence: true
+
+  validate :date_not_past, on: :create
+  validate :vacant, on: :create
+  validate :not_own
+
+  def date_not_past
+    errors.add(:date, 'cannot be in past') if date.past?
+  end
+
+  def vacant
+    errors.add(:base, 'already taken') if taken?
+  end
+
+  def not_own
+    errors.add(:base, 'you already own this space') if own?
+  end
+
+  private
+
+  def vacant?
+    !taken?
+  end
+
+  def taken?
+    Booking.exists? parking_space: parking_space, date: date
+  end
+
+  def own?
+    user.present? && parking_space&.owner == user
+  end
 end
